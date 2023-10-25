781a37be86354fb4e3c2f7342c09ae30e7f24809
neox56 <neox56@gmail.com>
2023-10-25 20:09:29 +0400

    lesson 3.routing in Laravel. Part 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/', function () {
+//    return '<h1>Hello, world!</h1>';
+//});
+
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'John';
+    $res = 2 + 3;
+//    return view('home')->with('var', $res);
+//    return view('home', ['var' => $res, 'name' => $name]);
+    return view('home', compact('name', 'res'));
+});
+
+Route::get('/about', function () {
+
+    return '<h1>About Page</h1>';
 });
